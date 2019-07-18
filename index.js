574a023ffc860d62a6a7fd41d145ba1fbe5b8cc4
Ron Gruesbeck <rongruesbeck@gmail.com>
2019-07-18 12:15:57 -0700

    add koji

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * index.js
+ * 
+ * What it Does:
+ *   It also loads the .internals/config.json which is a bundle of current customization
+ *   to the json files in the .koji directory
+ * 
+ * How to Use:
+ *   Make sure this file has a script tag in index.html
+ *   eg. <script src="./index.js"></script>
+ */
+
+// import and load koji configs
+import Koji from 'koji-tools'
+Koji.pageLoad()
+
+// set app name
+const config = Koji.config;
+
+// listen for config change events
+Koji.on('change', (scope, key, value) => {
+
+    // update configs and re-render
+    config[scope][key] = value
+    render();
+})
+
+// render app
+const render = () => {
+    document.body.innerHTML = `
+        <h1>
+            ${config.settings.name}
+        </h1>
+    `
+};
+
+render();
